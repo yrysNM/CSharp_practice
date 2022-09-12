fd0f8b35527ff1590b7804288aa6d5da3b349e29
yrysNM <yrys.n3m@gmail.com>
2022-09-12 10:21:07 +0600

    simple calculator version 0.02

---
+++ Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public bool isNewCalculations, isEnd;
+        public char arithmeticMethod;
+        public int firstNumber, secondNumber;
+
+        //properties
+        public int FirstNumber 
+        { 
+            get { return firstNumber; }
+            set { firstNumber = value; }
+        }
+
+        public int SecondNumber 
+        {
+            get { return secondNumber; }
+            set { secondNumber = value; }
+        }
+
+        public char ArithmeticMethod
+        {
+            get { return arithmeticMethod; }
+            set { arithmeticMethod = value; }
+        }
+
+
+        // constructor 
+        public Calculator(bool isNewCalculations, bool isEnd)
+        {
+            this.isNewCalculations = isNewCalculations;
+            this.isEnd = isEnd;
+        }
+
+           
+        // verification of which method is chosen
+        public void showResult()
+        {
+            switch (arithmeticMethod)
+            {
+                case '+':
+                    printValue(firstNumber + secondNumber);
+                    break;
+                case '-':
+                    printValue(firstNumber - secondNumber);
+                    break;
+                case '*':
+                    printValue(firstNumber * secondNumber);
+                    break;
+                case '/':
+                    printValue(firstNumber / secondNumber);
+                    break;
+                default:
+                    Console.WriteLine("Something is wrong");
+                    break;
+            }
+        }
+
+        public void printValue (int value)
+        {
+            Console.WriteLine($"Result: {value}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,45 +1,34 @@
 ﻿using System;
+using static SimpleCalculator.Calculator;
 
 namespace SimpleCalculator
 {
     class MainClass
     {
-            
-        public void printValue(int value) {
-            Console.WriteLine($"Result: {value}");
-        }
+       
+
 
         public static void Main(string[] args)
         {
+            //initial class
+            Calculator objCalculator = new Calculator(false, false);
+
+
             Console.WriteLine("Simple calculator in C#");
             Console.WriteLine("Enter first number: ");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
+            objCalculator.FirstNumber = firstNumber;
+
             Console.WriteLine("Enter second number: ");
             int secondNumber = Convert.ToInt32(Console.ReadLine());
+            objCalculator.SecondNumber = secondNumber;
 
             Console.WriteLine("Enter the arithmetic methods(+, -, *, /):\t ");
             char arithmeticMethod = Convert.ToChar(Console.ReadLine());
-            MainClass classMethods = new MainClass();
+            objCalculator.ArithmeticMethod = arithmeticMethod;
 
-
-            // verification of which method is chosen   
-            switch (arithmeticMethod) {
-                case '+':
-                    classMethods.printValue(firstNumber + secondNumber);
-                    break;
-                case '-':
-                    classMethods.printValue(firstNumber - secondNumber);
-                    break;
-                case '*':
-                    classMethods.printValue(firstNumber * secondNumber);
-                    break;
-                case '/':
-                    classMethods.printValue(firstNumber / secondNumber);
-                    break;
-                default:
-                    Console.WriteLine("Something is wrong");
-                    break;
-            }
+            objCalculator.showResult();
+         
         }
     }
 }
