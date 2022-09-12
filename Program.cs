b08636a211f28d7999589e8aac96fe33373d8ae8
yrysNM <yrys.n3m@gmail.com>
2022-09-12 09:21:35 +0600

    calculator version 0.01

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class MainClass
+    {
+            
+        public void printValue(int value) {
+            Console.WriteLine($"Result: {value}");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Simple calculator in C#");
+            Console.WriteLine("Enter first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the arithmetic methods(+, -, *, /):\t ");
+            char arithmeticMethod = Convert.ToChar(Console.ReadLine());
+            MainClass classMethods = new MainClass();
+
+
+            // verification of which method is chosen   
+            switch (arithmeticMethod) {
+                case '+':
+                    classMethods.printValue(firstNumber + secondNumber);
+                    break;
+                case '-':
+                    classMethods.printValue(firstNumber - secondNumber);
+                    break;
+                case '*':
+                    classMethods.printValue(firstNumber * secondNumber);
+                    break;
+                case '/':
+                    classMethods.printValue(firstNumber / secondNumber);
+                    break;
+                default:
+                    Console.WriteLine("Something is wrong");
+                    break;
+            }
+        }
+    }
+}
